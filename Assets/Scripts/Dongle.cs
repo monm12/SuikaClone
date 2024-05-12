dcbd0084f5711b7ac58c6ea15d5900c6e9e270dd
TaeGyun_Kim <134123228+monm12@users.noreply.github.com>
2024-05-12 18:41:33 +0900

    B54

---
+++ Assets/Scripts/Dongle.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dongle : MonoBehaviour
+{
+
+    public bool isDrag;
+    Rigidbody2D rigid;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+
+    }
+    void Update()
+    {
+        if (isDrag)
+        {
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            // x축 경계 설정
+            float leftBorder = -4.2f + transform.localScale.x / 2f;
+            float rightBorder = 4.2f - transform.localScale.x / 2f;
+
+            if (mousePos.x < leftBorder)
+            {
+                mousePos.x = leftBorder;
+            }
+            else if (mousePos.x > rightBorder)
+            {
+                mousePos.x = rightBorder;
+            }
+
+            mousePos.z = 0;
+            mousePos.y = 8;
+            transform.position = Vector3.Lerp(transform.position, mousePos, 0.2f);
+        }
+    }
+
+    public void Drag()
+    {
+        isDrag = true;
+
+    }
+    public void Drop()
+    {
+        isDrag = false;
+        rigid.simulated = true;
+    }
+}
+
+
